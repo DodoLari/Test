e1232e556563a3112caf1996b4f52a2d0bf3c86b
DodoLari <88659992+DodoLari@users.noreply.github.com>
2022-06-21 09:17:32 +0300

    Add files via upload

---
+++ AirConditioner.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp16
+{
+    public class AirConditioner
+    {
+        public int Now_temperatur {
+            get;
+            set;
+        }
+        public int blowingIntensity { get; set; }
+        public AirConditioner(int Now_temperatur, int blowingIntensity)
+        {
+            this.Now_temperatur = Now_temperatur;
+            this.blowingIntensity = blowingIntensity;
+        }
+        public void Inc_temperature()
+        {
+            if (Now_temperatur < 30)
+            {
+                Now_temperatur++;
+
+            }
+
+        }
+        public void Dec_temperature()
+        {
+            if (Now_temperatur > 30)
+            {
+                Now_temperatur--;
+            }
+
+        }
+        public void Dec_blowingIntensity()
+        {
+            if (blowingIntensity == 10)
+            {
+                blowingIntensity = 1;
+            }
+            else
+                blowingIntensity++;
+        }
+        public void Inc_blowingIntensity()
+        {
+            if (blowingIntensity == 1)
+            {
+                blowingIntensity = 10;
+            }
+            else
+                blowingIntensity++;
+        }
+    }
+}
